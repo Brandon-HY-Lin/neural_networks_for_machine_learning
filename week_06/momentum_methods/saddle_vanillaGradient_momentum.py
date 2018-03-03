7b9cde690dfd550e2ae9bb9447e0a8f796838ef8
brandon <brandon.hy.lin.0@gmail.com>
2018-03-04 00:23:18 +0800

    Add license comments

--- week_06/momentum_methods/saddle_vanillaGradient_momentum.py
+++ week_06/momentum_methods/saddle_vanillaGradient_momentum.py
@@ -1,3 +1,13 @@
+#Copyright 2018 Brandon H. Lin. {brandon.hy.lin.0@gamil.com}
+#
+#Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+#
+#http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+#
+#-------------------------------------------------------------------------
+
 # define cost function of saddle function (z = x^2 - y^2)
 # set initial point of (x, y)
 # define the families of gradient descent methods
